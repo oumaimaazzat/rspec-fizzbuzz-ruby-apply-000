b9d2228edbf74b68ee3b8f93db3c773c5d9c9ef9
Oumaima Azzat <oumaimazzat@gmail.com>
2020-09-14 14:48:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,20 @@
-def fizzbuzz(num)
-  if num % 15 == 0 
+def fizzbuzz(int)
+  if int
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    % 15 == 0 
     return "FizzBuzz"
   elsif num % 3  == 0 
     return "Fizz"
